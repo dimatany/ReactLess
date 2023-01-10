c001a033b77c7b6d1f225bcc1ffd335da7002e16
Dima Tatiana <Cheize2020@gmail.com>
2023-01-10 17:40:32 +0200

    first1

--- src/components/StocksF/Stocks.js
+++ src/components/StocksF/Stocks.js
@@ -3,6 +3,9 @@ import styles from './Stocks.module.css'
 import Countdown from '../CountdownF/Countdown';
 import Video from "./video.mp4";
 
+import {BrowserRouter as Router, Route, Routes, Link} from 'react-router-dom';
+
+
 function Stocks(props) {
 	return (
 		<>
@@ -19,6 +22,9 @@ function Stocks(props) {
 			<div className={styles.video}>
 				<video src={Video} width="600" height="300" controls="controls" autoPlay="true"/>
 			</div>
+			<div className={styles.transition}>
+				<a href="/goods">{props.transition}</a>
+			</div>
 		</>
 	)
 }
@@ -29,5 +35,6 @@ Stocks.defaultProps = {
 	description: 'Знижки до 40% на всі продукти Lador.',
 	period: 'Період проведення акції з 9 грудня по 31 грудня 2022 року',
 	reference: 'Поспішайте! Кількість товарів обмежена!',
+	transition: 'Щоб замовити товар, натисніть на цей напис та перейдіть на сторінку замовлення.',
 };
 export default Stocks;
