e0484030a9efc931a72e46102f3ebb029a231db6
Dima Tatiana <Cheize2020@gmail.com>
2023-01-23 17:44:31 +0200

    first1

--- src/components/WhatWeDoF/WhatWeDoDetail.js
+++ src/components/WhatWeDoF/WhatWeDoDetail.js
@@ -11,29 +11,64 @@ function WhatWeDoDetail(props) {
 		(el) => el.id === params.whatWeDoDetail
 	);
 	
-	const { title, subtitle1, subtitlePrice1, subSubTitle1 } = currentWWD;
+	const { title, subtitle1, subtitlePrice1, subSubTitle1, subtitle2, subtitlePrice2, subSubTitle2, subtitle3, subtitlePrice3, subSubTitle3, subtitle4, subtitlePrice4, subSubTitle4, image, image1, image2, image3, image4, alt } = currentWWD;
 	
 	return (
 		<>
 			<div className={styles.wrap}>
 				<h2 className={styles.wrapTitle}>{title}</h2>
 				<Descriptions text={props.text2}/>
-				<div>
-					{dataItems.map(el =>
-						<div key={el.id} className={styles.infoDescr}>
-							<div className={styles.infoImage} key={el.id}>
-								<img key={el.id} src={el.image} alt={el.alt}/>
-							</div>
-							<div className={styles.infoDetails}>
-								<details>
-									<summary>{subtitle1}</summary>
-									<p>{subSubTitle1}</p>
-									<span>{subtitlePrice1}</span>
-									<span className={styles.currency}>{props.currency}</span>
-								</details>
-							</div>
-						</div>
-					)}
+				<div className={styles.infoBlock}>
+					<div className={styles.infoImage}>
+						<img src={image1} alt={alt}/>
+					</div>
+					<div className={styles.infoDetails}>
+						<details>
+							<summary>{subtitle1}</summary>
+							<p>{subSubTitle1}</p>
+							<span>{subtitlePrice1}</span>
+							<span className={styles.currency}>{props.currency}</span>
+						</details>
+					</div>
+				</div>
+				<div className={styles.infoBlock}>
+					<div className={styles.infoImage}>
+						<img src={image2} alt={alt}/>
+					</div>
+					<div className={styles.infoDetails}>
+						<details>
+							<summary>{subtitle2}</summary>
+							<p>{subSubTitle2}</p>
+							<span>{subtitlePrice2}</span>
+							<span className={styles.currency}>{props.currency}</span>
+						</details>
+					</div>
+				</div>
+				<div className={styles.infoBlock}>
+					<div className={styles.infoImage}>
+						<img src={image3} alt={alt}/>
+					</div>
+					<div className={styles.infoDetails}>
+						<details>
+							<summary>{subtitle3}</summary>
+							<p>{subSubTitle3}</p>
+							<span>{subtitlePrice3}</span>
+							<span className={styles.currency}>{props.currency}</span>
+						</details>
+					</div>
+				</div>
+				<div className={styles.infoBlock}>
+					<div className={styles.infoImage}>
+						<img src={image4} alt={alt}/>
+					</div>
+					<div className={styles.infoDetails}>
+						<details>
+							<summary>{subtitle4}</summary>
+							<p>{subSubTitle4}</p>
+							<span>{subtitlePrice4}</span>
+							<span className={styles.currency}>{props.currency}</span>
+						</details>
+					</div>
 				</div>
 				<Descriptions text={props.text}/>
 			</div>
@@ -45,6 +80,6 @@ function WhatWeDoDetail(props) {
 WhatWeDoDetail.defaultProps = {
 	currency: 'грн',
 	text: 'У нас є багато різних послуг в асортименті - якщо ви хочете зареєструватися на консультацію по номерах або електронній пошті, наданих в капелюсі сайту',
-	text2: 'Ми надаємо послуги популяції по догляду за волоссям (різання, створення волосся, фарбування, танення та іншого фарбування, стрижки, гоління та стрижки, стрижка та стрижка бороди та вус тощо) в номері обладнаному спеціально для цього.',
+	text2: 'Ми надаємо послуги популяції по догляду за волоссям (різання, упряжування, створення волосся, фарбування, танення та іншого фарбування, стрижки, гоління та стрижки, стрижка та стрижка бороди та вус тощо) в номері обладнаному спеціально для цього.',
 };
 export default WhatWeDoDetail;
