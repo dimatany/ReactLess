277d8b031ef262078b9ad734ccf1b71a20552e3a
Dima Tatiana <Cheize2020@gmail.com>
2023-01-25 17:09:14 +0200

    first1

--- src/components/NavigationF/NavHeader.js
+++ src/components/NavigationF/NavHeader.js
@@ -6,23 +6,25 @@ function NavHeader(props) {
 	return (
 		<>
 			<header className={styles.header}>
-				<a href="#" className={styles.logo}>
-					<SVGAvatar fill='white'/>
-				</a>
-				<input className={styles.sideMenu} type="checkbox" id="sideMenu"/>
-				<label className={styles.hamb} htmlFor="sideMenu">
-					<span className={styles.hambLine}></span>
-				</label>
-				<nav className={styles.nav}>
-					<ul className={styles.menu}>
-						<li><a href="/about">Головна</a></li>
-						<li><a href="/masters">Майстри</a></li>
-						<li><a href="/goods">Товари</a></li>
-						<li><a href="/stocks">Акції</a></li>
-						<li><a href="/blog">Блог</a></li>
-						<li><a href="/contacts">Контакти</a></li>
-					</ul>
-				</nav>
+				<div className={styles.wrap}>
+					<a href="#" className={styles.logo}>
+						<SVGAvatar fill='white'/>
+					</a>
+					<input className={styles.sideMenu} type="checkbox" id="sideMenu"/>
+					<label className={styles.hamb} htmlFor="sideMenu">
+						<span className={styles.hambLine}></span>
+					</label>
+					<nav className={styles.nav}>
+						<ul className={styles.menu}>
+							<li><a href="/about">Головна</a></li>
+							<li><a href="/masters">Майстри</a></li>
+							<li><a href="/goods">Товари</a></li>
+							<li><a href="/stocks">Акції</a></li>
+							<li><a href="/blog">Блог</a></li>
+							<li><a href="/contacts">Контакти</a></li>
+						</ul>
+					</nav>
+				</div>
 			</header>
 		</>
 	);
