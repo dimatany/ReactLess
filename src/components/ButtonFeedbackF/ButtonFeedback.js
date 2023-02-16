071268c0947cc1be036136abdcc83c1aaa2bea62
Dima Tatiana <Cheize2020@gmail.com>
2023-02-16 13:01:19 +0200

    first1

--- src/components/ButtonFeedbackF/ButtonFeedback.js
+++ src/components/ButtonFeedbackF/ButtonFeedback.js
@@ -1,18 +1,20 @@
 import React from 'react';
 import styles from './ButtonFeedback.module.css'
 import SVGLetter from '../SVG/SVGLetter';
+import Modal from '../ModalF/Modal';
+import useModal from '../ModalF/useModal';
 
 function ButtonFeedback(props) {
+	const {isShowing, toggle} = useModal();
 	return (
 		<section className={styles.wrap}>
-			<div role="button" tabIndex="0" className={styles.wrapButton}>
+			<div role="button" onClick={toggle} tabIndex="0" className={styles.wrapButton}>
 				<div className={styles.buttonAria}>
-					<div className={styles.buttonPosition}>
-						<div className={styles.buttonWrapSVG}>
-							<SVGLetter fill='#5383ed'/>
-						</div>
+					<div className={styles.buttonWrapSVG}>
+						<SVGLetter fill='#5383ed'/>
 					</div>
 				</div>
+				<Modal isShowing={isShowing} hide={toggle}/>
 			</div>
 		</section>
 	);
