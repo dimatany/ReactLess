1533cc180613875799a71043188f0f1899491b19
Dima Tatiana <Cheize2020@gmail.com>
2023-02-24 15:48:42 +0200

    first1

--- src/components/ButtonFeedbackF/ButtonFeedback.js
+++ src/components/ButtonFeedbackF/ButtonFeedback.js
@@ -1,25 +1,27 @@
 import React from 'react';
 import styles from './ButtonFeedback.module.css'
 import SVGLetter from '../SVG/SVGLetter';
-import ModalWindow from '../ModalF/ModalWindow';
+import Modal from '../../components/ModalAllF/Modal';
 import FormTelegram from '../FormTelegramF/FormTelegram';
 
+
 function ButtonFeedback(props) {
-	const [isModal, setModal] = React.useState(false);
+	const [modalOpen, setModalOpen] = React.useState(false);
 	return (
 		<section className={styles.wrap}>
 			<div className={styles.wrapButton}>
 				<div className={styles.buttonAria}>
-					<div onClick={() => setModal(true)} className={styles.buttonWrapSVG}>
+					<div onClick={() => setModalOpen(true)} className={styles.buttonWrapSVG}>
 						<SVGLetter fill='#5383ed'/>
 					</div>
 				</div>
-				<ModalWindow
-					isVisible={isModal}
-					title="Modal Title"
-					content={<p><FormTelegram/></p>}
-					onClose={() => setModal(false)}
-				/>
+				{modalOpen && (
+					<Modal
+						isVisible={modalOpen}
+						content={<p><FormTelegram/></p>}
+						onClose={() => setModalOpen(false)}
+					/>
+				)}
 			</div>
 		</section>
 	);
---
+++ src/components/ModalAllF/Modal.js
@@ -0,0 +1,25 @@
+import React from "react";
+import useLockBodyScroll from "./useLockBodyScroll";
+import styles from './Modal.module.css'
+import SVGChange from '../SVG/SVGChange';
+
+function Modal({ content, onClose }) {
+	//call hook to lock body scroll
+	useLockBodyScroll();
+	return (
+		<div className={styles.modal} onClick={onClose}>
+			<div className={styles.modalDialog} onClick={e => e.stopPropagation()}>
+				<div className={styles.modalHeader}>
+					<span className={styles.modalClose} onClick={onClose}>
+						<SVGChange fill='#5383ed'/>
+					</span>
+				</div>
+				<div className={styles.modalBody}>
+					<div className={styles.modalContent}>{content}</div>
+				</div>
+			</div>
+		</div>
+	);
+}
+
+export default Modal;
