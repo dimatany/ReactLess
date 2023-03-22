1c6e5f3d6a153314e7bdbd0b863fa3b6132aaf76
Dima Tatiana <Cheize2020@gmail.com>
2023-03-22 11:52:48 +0200

    first1

--- src/components/MastersF/MastersDetail.js
+++ src/components/MastersF/MastersDetail.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
 import ErrorBoundary from '../ErrorBoundaryC/ErrorBoundary';
-import data from '../../BLL/Data'
+import data from '../../BLL/Data';
 import styles from '../MastersF/MasterDetail.module.css';
 import Headings from '../HeadingsF/Headings';
 import DetailPost from './DetailPost';
