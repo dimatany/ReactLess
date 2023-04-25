44855be9a54397534afe871ed190fad361b4da7c
Dima Tatiana <Cheize2020@gmail.com>
2023-04-25 13:26:48 +0300

    first1

--- src/components/BlogF/BlogPost.js
+++ src/components/BlogF/BlogPost.js
@@ -1,24 +1,42 @@
-import React from 'react';
+import React, { useState } from "react";
 import styles from './BlogPost.module.css';
 
 
 
-function BlogPost({ notes }) {
+function BlogPost({ notes, ...props }) {
+	const [showMore, setShowMore] = useState(0);
+	const handelShow = (id) => {
+		setShowMore(id);
+	};
 	return (
 		<>
-			{notes.map(el=>
+			{notes.map((el) => { const isTestOpen = el.id === showMore;
+			return (
 				<div key={el.id} className={styles.wrap}>
 					<div className={styles.wrapPost}>
 						<span className={styles.postNumber}>{el.id}</span>
 						<div className={styles.postContent}>
 							<p className={styles.postContentHeader}>{el.header}</p>
-							<p className={styles.postContentText}>{el.text}</p>
+							<p className={styles.postContentText}>{el.text}
+								{isTestOpen
+									? el.text
+									: el.text.substring(0, 200) + "..."}
+							</p>
+							{isTestOpen ? (
+								<p className={styles.less} onClick={() => handelShow(0)}>{props.buttonLess}</p>
+							) : (
+								<p className={styles.more} onClick={() => handelShow(el.id)} >{props.buttonMore}</p>)}
 						</div>
 					</div>
 				</div>
-			)}
+			);
+			})}
 		</>
 	);
 }
+BlogPost.defaultProps = {
+	buttonLess: '"Показати менше..."',
+	buttonMore: '"Показати більше..."',
+};
 
 export default BlogPost;
