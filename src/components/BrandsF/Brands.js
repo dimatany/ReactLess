853c8f68854bb13fbb7b0f5cf2c89fb837079fa1
Dima Tatiana <Cheize2020@gmail.com>
2023-05-04 16:02:01 +0300

    first1

--- src/components/BrandsF/Brands.js
+++ src/components/BrandsF/Brands.js
@@ -9,10 +9,22 @@ function Brands() {
 	return (
 		<div className={styles.partners}>
 			<ul>
-				<li><img src={require("../../BLL/img/Brands/logoBrand1.png")} alt='logo of partners'/></li>
-				<li><img src={require("../../BLL/img/Brands/logoBrand2.png")} alt='logo of partners'/></li>
-				<li><img src={require("../../BLL/img/Brands/logoBrand3.png")} alt='logo of partners'/></li>
-				<li><img src={require("../../BLL/img/Brands/logoBrand4.png")} alt='logo of partners'/></li>
+				<li>
+					<img src={require("../../BLL/img/Brands/logoBrand1.png")}
+					     alt='logo of partners'/>
+				</li>
+				<li>
+					<img src={require("../../BLL/img/Brands/logoBrand2.png")}
+					     alt='logo of partners'/>
+				</li>
+				<li>
+					<img src={require("../../BLL/img/Brands/logoBrand3.png")}
+					     alt='logo of partners'/>
+				</li>
+				<li>
+					<img src={require("../../BLL/img/Brands/logoBrand4.png")}
+					     alt='logo of partners'/>
+				</li>
 			</ul>
 		</div>
 	);
