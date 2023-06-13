9de790ff2a9447b9bee3551e8bc3b7b310aad31a
Dima Tatiana <Cheize2020@gmail.com>
2023-06-13 14:12:09 +0300

    fix bug path="/" element

--- src/Сomponents/GoodsCard/GoodsCard.js
+++ src/Сomponents/GoodsCard/GoodsCard.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import styles from './GoodsCard.module.css';
 import {Link} from 'react-router-dom';
 import SVGTrolley from '../SVG/SVGTrolley';
