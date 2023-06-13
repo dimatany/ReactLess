64ad32f62cae4c78dd0056738c2b1e9ed80faca9
Dima Tatiana <Cheize2020@gmail.com>
2023-06-13 15:47:31 +0300

    adding code from the react-animations

--- src/Сomponents/MasterDetailPost/MasterDetailPost.js
+++ src/Сomponents/MasterDetailPost/MasterDetailPost.js
@@ -1,24 +1,24 @@
 import React from 'react';
 import styles from './MasterDetailPost.module.css';
 
-import {slideInLeft} from 'react-animations';
+import { fadeIn } from 'react-animations';
 import Radium, {StyleRoot} from 'radium';
 
+const style = {
+	fadeIn  : {
+		animation: 'x 4s',
+		animationName: Radium.keyframes(fadeIn, 'fadeIn'),
+	}
+};
+
 function MasterDetailPost(props) {
-	const style = {
-		slideInLeft : {
-			animation: 'x 2s',
-			animationName: Radium.keyframes(slideInLeft, 'slideInLeft'),
-		}
-	};
-	
 	return (
 		<div className={styles.wrapBlock}>
 			<h4 className={styles.wrapTitle}>{props.subTitle}</h4>
 			<div className={styles.wrapDescription}>
 				<div className={styles.descriptionImg}>
 					<StyleRoot>
-						<div className="test" style={style.slideInLeft}>
+						<div className="test" style={style.fadeIn}>
 					<img src={props.image} alt={props.alt}/>
 						</div>
 					</StyleRoot>
