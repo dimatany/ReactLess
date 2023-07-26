c832b98b7b5d3c3978383c4dac3ba47d375b1df3
Dima Tatiana <Cheize2020@gmail.com>
2023-07-26 20:04:47 +0300

    fix bug

--- src/Сomponents/FormLoginSingUp/FormLoginSingUp.js
+++ src/Сomponents/FormLoginSingUp/FormLoginSingUp.js
@@ -1,4 +1,5 @@
 import React, {useState} from 'react';
+import { useForm } from "react-hook-form";
 import styles from './FormLoginSingUp.module.css';
 
 
@@ -7,11 +8,20 @@ function FormLoginSingUp({handleClick ,...props}) {
 	const [email, setEmail] = useState('');
 	const [password, setPassword] = useState('');
 	
+	const {
+		register,
+		handleSubmit,
+		formState: {errors, isValid},
+		reset,
+	} = useForm({
+		mode: 'onBlur',
+	});
 	
 	const onSubmit = async (form, event) => {
 		event.preventDefault();
 		if (error) {
 			setError("Помилка даних! Перевірте данні та пробуйте знову!");
+			reset();
 		}
 	};
 	
@@ -19,23 +29,34 @@ function FormLoginSingUp({handleClick ,...props}) {
 	return (
 		<>
 			{error && <p>{error}</p>}
-			<form className={styles.form} onSubmit={onSubmit}>
+			<form className={styles.form} onSubmit={handleSubmit(onSubmit)}>
 				<div className={styles.wrapForm}>
 					<label>Ваш Email: name@gmail.com
 						<input
+							{...register('email', {
+								required: 'Це поле потрібно заповнити!',
+								pattern: {
+									value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+									message: "Додайте адресу у встановленому форматі!"
+								}
+							})}
 							type="email"
 							autoComplete='email'
 							onChange={(e) => setEmail(e.target.value)}
 						/>
+						{errors.email && <span style={{ color: 'red' }}>
+							Додайте адресу у встановленому форматі!</span>}
 					</label>
 					<label>Ваш пароль:
 						<input
+							{...register('password')}
 							type='password'
 							autoComplete='current-password'
 							onChange={(e) => setPassword(e.target.value)}
 						/>
 					</label>
 					<input type={'submit'}
+					       disabled={!isValid}
 					       value={props.text}
 					       className={styles.btn}
 					       onClick={() => handleClick(email, password)}
